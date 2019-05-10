86e9f22016810d30d588cb56b797b2d7bd12fb73
cesarcast1llo <cesarcast1llo@yahoo.com>
2019-05-10 16:29:08 -0700

    formula add to convert K to F for degrees

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,7 +5,7 @@ const Weather = (props) => {
     <div class="col-12 output_weather">
 
         {props.country && props.city && <p>Location: {props.city}, {props.country}</p>}
-        {props.temperature && <p>Temperature: {props.temperature}</p>}
+        {props.temperature && <p>Temperature: {(props.temperature * 9 / 5 - 459.67).toFixed(1)} F˚</p> }
         {props.humidity && <p>Humidity: {props.humidity}%</p>}
         {props.description && <p>Conditions:  {props.description}</p>}
         {props.error && <p>{props.error}</p>}
